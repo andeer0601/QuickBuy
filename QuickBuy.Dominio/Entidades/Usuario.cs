19fbac26a464c9c7d583b439b7dc5c41f503dcf8
Anderson <48699535+andeer0601@users.noreply.github.com>
2021-04-17 15:54:39 -0300

    Criacao das Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public int Email { get; set; }
+        public int Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Usuario pode ter Nenhum ou n pedidos 
+        /// </summary>
+
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,32 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
